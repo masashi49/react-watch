ed7d6281c45e94980a49f98df2030f9a712200f5
yamada <yamada@reas.jp>
2019-11-27 14:06:37 +0900

    Storeの作成まで

--- redux-calculator/src/index.js
+++ redux-calculator/src/index.js
@@ -1,12 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import CalculatorContainer from './containers/CalculatorContainer';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<CalculatorContainer />, document.getElementById('root'));
+import './index.css';
+import React from 'react';
+import {render} from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import CalculatorContainer from './containers/CalculatorContainer';
+import reducer from './reducers';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const store = createStore(reducer);
+
+render(
+    <Provider store={store}>
+        <CalculatorContainer/>
+    </Provider>,
+    document.getElementById('root')
+);
 serviceWorker.unregister();
--- redux-calculator/src/reducers/calculator.js
+++ redux-calculator/src/reducers/calculator.js
@@ -1,27 +1,28 @@
 import * as actionTypes from '../utils/actionTypes';
 
-//管理しておきたいデータ軍
 const initialAppState = {
   inputValue: 0,
   resultValue: 0,
   showingResult: false,
-}
+};
 
 const calculator = (state = initialAppState, action) => {
   if (action.type === actionTypes.INPUT_NUMBER) {
     return {
       ...state,
-      inputValue: state.inputValue * 10 + action.number
-    }
-  } else if (action.type === actionTypes.PULUS) {
+      inputValue: state.inputValue * 10 + action.number,
+      showingResult: false,
+    };
+  } else if (action.type === actionTypes.PLUS) {
     return {
       ...state,
       inputValue: 0,
       resultValue: state.resultValue + state.inputValue,
-      showingResult: true;
+      showingResult: true,
+    };
+  } else {
+    return state;
   }
-  }
-  return state
-}
+};
 
-export default  calculator()
+export default calculator;
