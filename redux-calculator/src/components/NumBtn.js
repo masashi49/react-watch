392b95240969b48da970e431b5393fd8969138fa
山田将史 <13416062+masashi49@users.noreply.github.com>
2019-11-28 01:09:02 +0900

    no message

--- redux-calculator/src/components/NumBtn.js
+++ redux-calculator/src/components/NumBtn.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const NumBtn = ({n}) => (
-  <button>{n}</button>
-)
+const NumBtn = ({n, onClick}) => (
+  <button onClick={onClick}>{n}</button>
+);
 
-export default NumBtn
+NumBtn.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+export default NumBtn;
